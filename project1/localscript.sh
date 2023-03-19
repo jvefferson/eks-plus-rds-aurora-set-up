2c985c76fcb64236116e50f213d91098a4a9aee1
jvefferson <jvefferson@gmail.com>
2023-03-19 17:45:05 +0000

    added eks module

--- project1/localscript.sh
+++ project1/localscript.sh
@@ -7,6 +7,7 @@ mv ./kubectl ~/.local/bin/kubectl
 # Install aws authenticator
 #curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.5.9/aws-iam-authenticator_0.5.9_darwin_amd64
 #curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.5.9/aws-iam-authenticator_0.5.9_darwin_arm64
+#curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
 #chmod +x aws-iam-authenticator
 #sudo mv aws-iam-authenticator /bin
 #aws-iam-authenticator version
--- project1/terraform.auto.tfvars
+++ project1/terraform.auto.tfvars
@@ -1,6 +1,6 @@
 region = "us-east-1"
 cluster_name = "my-cluster"
-cluster_version = "1.21"
+cluster_version = "1.24"
 instance_type = "m4.large"
 asg_max_size = 99
 asg_min_size = 1
