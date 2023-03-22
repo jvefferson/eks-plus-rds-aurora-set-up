487698bb633d19bce53aa5c53e3baded25f61ab3
jvefferson <107517235+jvefferson@users.noreply.github.com>
2023-03-22 18:29:34 -0500

    added S3 bucket name

--- RDS/data.tf
+++ RDS/data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "remote" {
   backend = "s3"
   config = {
-    bucket = "terraform-backend-julia123"
+    bucket = "terraform-backend-for-release"
     key    = "env:/dev/path/to/my/key"
     region = "us-east-1"
     }
