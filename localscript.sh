f422425d1bc1c33980a0c461f0ffaf34cd37646e
jvefferson <107517235+jvefferson@users.noreply.github.com>
2023-03-21 19:08:45 -0500

    Update localscript.sh

--- localscript.sh
+++ localscript.sh
@@ -6,8 +6,8 @@ sudo ln -s ~/.tfenv/bin/* /usr/local/bin
 source ~/.bash_profile
 
 # RUN
-# tfenv install 1.1.1 
-# tfenv use 1.1.1 
+# tfenv install 1.4.2 
+# tfenv use 1.4.2 
 # terraform version
 
 # bash localscript.sh
