262fb8152b7065016ade56659ede74c072e1d7e1
jvefferson <107517235+jvefferson@users.noreply.github.com>
2023-03-21 19:07:03 -0500

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -1,42 +1,125 @@
-data "aws_availability_zones" "all" {}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
 
-resource "aws_default_subnet" "default_az1" {
-        availability_zone = data.aws_availability_zones.all.names[0]
-}
+  cluster_name    = "my-cluster"
+  cluster_version = "1.24"
 
-resource "aws_default_subnet" "default_az2" {
-        availability_zone = data.aws_availability_zones.all.names[1]
-}
+  cluster_endpoint_public_access  = true
 
-resource "aws_default_subnet" "default_az3" {
-        availability_zone = data.aws_availability_zones.all.names[2]
-}
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
+  vpc_id                   = "vpc-02833d0be7d51c897"
+  subnet_ids               = ["subnet-0cd4f6d02a45666b3", "subnet-072c9f01b75a5503f", "subnet-01b2db30a2d5f8492"]
+  control_plane_subnet_ids = ["subnet-0cd4f6d02a45666b3", "subnet-072c9f01b75a5503f", "subnet-01b2db30a2d5f8492"]
 
-data "aws_eks_cluster" "cluster" {
-        name = module.my-cluster.cluster_id
-}
-data "aws_eks_cluster_auth" "cluster" {
-        name = module.my-cluster.cluster_id
-}
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    instance_type                          = "m6i.large"
+    update_launch_template_default_version = true
+    iam_role_additional_policies = {
+      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    }
+  }
 
-module "my-cluster" {
-        source = "terraform-aws-modules/eks/aws"
-        version = "15.0.0"
-        cluster_name = var.cluster_name
-        cluster_version = var.cluster_version
-        subnets = [
-                aws_default_subnet.default_az1.id,
-                aws_default_subnet.default_az2.id,
-                aws_default_subnet.default_az3.id,
+  self_managed_node_groups = {
+    one = {
+      name         = "mixed-1"
+      max_size     = 5
+      desired_size = 2
+
+      use_mixed_instances_policy = true
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity                  = 0
+          on_demand_percentage_above_base_capacity = 10
+          spot_allocation_strategy                 = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "m5.large"
+            weighted_capacity = "1"
+          },
+          {
+            instance_type     = "m6i.large"
+            weighted_capacity = "2"
+          },
         ]
-        vpc_id = aws_default_subnet.default_az3.vpc_id
-        cluster_create_security_group = true
-        worker_groups = [{
-            instance_type = var.instance_type
-            asg_max_size = var.asg_max_size
-            asg_min_size = var.asg_min_size #min size should be 1
-                }
-        ]
-        tags = var.tags
+      }
+    }
+  }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Fargate Profile(s)
+#   fargate_profiles = {
+#     default = {
+#       name = "default"
+#       selectors = [
+#         {
+#           namespace = "default"
+#         }
+#       ]
+#     }
+#   }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = false
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::655832699841:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::655832699841:user/user1"
+      username = "user1"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::655832699841:user/user2"
+      username = "user2"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_accounts = [
+    "777777777777",
+    "888888888888",
+  ]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
